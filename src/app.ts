423dae2572fdf857641fa6a30667212f1da74722
likhon29 <mdabdurrouf.likhon@gmail.com>
2023-11-23 13:48:19 +0600

    project setup done

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
+
+const app: Application = express();
+
+//parsers
+app.use(express.json());
+app.use(cors());
+
+const getAController = (req: Request, res: Response) => {
+  const a = 10;
+  res.send(a);
+};
+
+app.get('/', getAController);
+
+export default app;
